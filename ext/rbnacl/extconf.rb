479be17ae478bf01f7764b9e1bbc31d1e8993921
Tony Arcieri <tony.arcieri@gmail.com>
2012-12-28 20:46:47 -0800

    Check crypto_nacl_base when linking

--- ext/rbnacl/extconf.rb
+++ ext/rbnacl/extconf.rb
@@ -3,6 +3,6 @@ require 'fileutils'
 
 dir_config 'rbnacl_ext'
 
-if have_header("crypto_secretbox.h") && have_library("nacl", "randombytes")
+if have_header("crypto_secretbox.h") && have_library("nacl", "crypto_nacl_base")
   create_makefile 'rbnacl_ext'
 end
