8f90e3988515ad8c124a4ba58f7c2f3808826ca5
Jonathan Stott <jonathan.stott@gmail.com>
2013-01-20 22:23:18 +0000

    Now using libsodium
    Hooray, we now have a proper portable distribution of NaCl to use, so
    this changes over the C extension to use that.  The next step is to
    ffi-ize the library, so that it works on jruby or anywhere.

--- ext/rbnacl/rbnacl_ext.c
+++ ext/rbnacl/rbnacl_ext.c
@@ -5,11 +5,10 @@
  * LICENSE.txt for further details.
  */
 
-#include <crypto_hash_sha256.h>
-#include <crypto_box.h>
+#include <sodium/crypto_hash_sha256.h>
+#include <sodium/crypto_box.h>
 
 #include "ruby.h"
-#include "extconf.h"
 
 static VALUE mCrypto = Qnil;
 static VALUE mCrypto_Hash = Qnil;
@@ -169,39 +168,3 @@ static VALUE Crypto_Boxer_unbox(VALUE self, VALUE nonce, VALUE ct)
 }
 
 
-#if RBNACL_NEED_RANDOM_BYTES
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-/* it's really stupid that there isn't a syscall for this */
-
-static int fd = -1;
-
-void randombytes(unsigned char *x,unsigned long long xlen)
-{
-  int i;
-
-  if (fd == -1) {
-    for (;;) {
-      fd = open("/dev/urandom",O_RDONLY);
-      if (fd != -1) break;
-      sleep(1);
-    }
-  }
-
-  while (xlen > 0) {
-    if (xlen < 1048576) i = xlen; else i = 1048576;
-
-    i = read(fd,x,i);
-    if (i < 1) {
-      sleep(1);
-      continue;
-    }
-
-    x += i;
-    xlen -= i;
-  }
-}
-#endif
