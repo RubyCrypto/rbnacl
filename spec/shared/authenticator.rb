09f63ac170000d7c012890fa9d4ad19b82061f72
Jonathan Stott <jonathan.stott@gmail.com>
2013-01-26 21:02:10 +0000

    Rename authenticator specs in prep for one time
    This allows for greater consistency between all the authenticators!

--- spec/shared/authenticator.rb
+++ spec/shared/authenticator.rb
@@ -1,7 +1,7 @@
-shared_examples "auth_hmac" do
+shared_examples "authenticator" do
   context ".new" do
     it "accepts a key" do
-      expect { described_class.new(rfc_key)  }.to_not raise_error(ArgumentError)
+      expect { described_class.new(key)  }.to_not raise_error(ArgumentError)
     end
 
     it "requires a key" do
@@ -19,85 +19,85 @@ shared_examples "auth_hmac" do
 
   context ".auth" do
     it "produces an authenticator" do
-      described_class.auth(rfc_key, rfc_data).should eq rfc_result
+      described_class.auth(key, message).should eq tag
     end
 
     it "raises on a nil key" do
-      expect { described_class.auth(nil, rfc_data)  }.to raise_error(ArgumentError)
+      expect { described_class.auth(nil, message)  }.to raise_error(ArgumentError)
     end
 
     it "raises on a key which is too long" do
-      expect { described_class.auth("\0"*33, rfc_data)  }.to raise_error(ArgumentError)
+      expect { described_class.auth("\0"*33, message)  }.to raise_error(ArgumentError)
     end
   end
 
   context ".verify" do
     it "verify an authenticator" do
-      described_class.verify(rfc_key, rfc_result, rfc_data).should eq true
+      described_class.verify(key, tag, message).should eq true
     end
 
     it "raises on a nil key" do
-      expect { described_class.verify(nil, rfc_result, rfc_data)  }.to raise_error(ArgumentError)
+      expect { described_class.verify(nil, tag, message)  }.to raise_error(ArgumentError)
     end
 
     it "raises on a key which is too long" do
-      expect { described_class.verify("\0"*33, rfc_result, rfc_data)  }.to raise_error(ArgumentError)
+      expect { described_class.verify("\0"*33, tag, message)  }.to raise_error(ArgumentError)
     end
 
     it "fails to validate an invalid authenticator" do
-      described_class.verify(rfc_key, rfc_result, rfc_data+"\0").should be false
+      described_class.verify(key, tag, message+"\0").should be false
     end
     it "fails to validate a short authenticator" do
-      described_class.verify(rfc_key, rfc_result[0,31], rfc_data).should be false
+      described_class.verify(key, tag[0,tag.bytesize - 2], message).should be false
     end
     it "fails to validate a long authenticator" do
-      described_class.verify(rfc_key, rfc_result+"\0", rfc_data).should be false
+      described_class.verify(key, tag+"\0", message).should be false
     end
   end
 
 
   context "Instance methods" do
-    let(:hmac) { described_class.new(rfc_key) }
-    let(:rfc_hex) { Crypto::Util.hexencode(rfc_result)  }
+    let(:authenticator) { described_class.new(key) }
+    let(:hex) { Crypto::Util.hexencode(tag)  }
     context "#auth" do
       it "produces an authenticator" do
-        hmac.auth(rfc_data).should eq rfc_result
+        authenticator.auth(message).should eq tag
       end
     end
 
     context "#hexauth" do
       it "produces a hex encoded authenticator" do
-        hmac.hexauth(rfc_data).should eq rfc_hex
+        authenticator.hexauth(message).should eq hex
       end
     end
 
     context "#verify" do
       it "verifies an authenticator" do
-        hmac.verify(rfc_result, rfc_data).should be true
+        authenticator.verify(tag, message).should be true
       end
       it "fails to validate an invalid authenticator" do
-        hmac.verify(rfc_result, rfc_data+"\0").should be false
+        authenticator.verify(tag, message+"\0").should be false
       end
       it "fails to validate a short authenticator" do
-        hmac.verify(rfc_result[0,31], rfc_data).should be false
+        authenticator.verify(tag[0,tag.bytesize - 2], message).should be false
       end
       it "fails to validate a long authenticator" do
-        hmac.verify(rfc_result+"\0", rfc_data).should be false
+        authenticator.verify(tag+"\0", message).should be false
       end
     end
-    
+
     context "#hexverify" do
       it "verifies an hexencoded authenticator" do
-        hmac.hexverify(rfc_hex, rfc_data).should be true
+        authenticator.hexverify(hex, message).should be true
       end
       it "fails to validate an invalid authenticator" do
-        hmac.hexverify(rfc_hex, rfc_data+"\0").should be false
+        authenticator.hexverify(hex, message+"\0").should be false
       end
       it "fails to validate a short authenticator" do
-        hmac.hexverify(rfc_hex[0,62], rfc_data).should be false
+        authenticator.hexverify(hex[0,hex.bytesize - 2], message).should be false
       end
       it "fails to validate a long authenticator" do
-        hmac.hexverify(rfc_hex+"00", rfc_data).should be false
+        authenticator.hexverify(hex+"00", message).should be false
       end
     end
   end
