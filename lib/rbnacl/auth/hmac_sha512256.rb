44ba9e95dbb93e2aea148df53aa30d210f1ae6f0
Jonathan Stott <jonathan.stott@gmail.com>
2013-01-27 12:05:51 +0000

    Use message, not data for arguments
    This is more consistent with the specs, and also with the NaCl
    documentation, which is now linked.

--- lib/rbnacl/auth/hmac_sha512256.rb
+++ lib/rbnacl/auth/hmac_sha512256.rb
@@ -2,13 +2,15 @@ module Crypto
   module Auth
     # Computes an authenticator as HMAC-SHA-512 truncated to 256-bits
     #
-    # The authenticator can be used at a later data to verify the provenence of
-    # the data by recomputing the HMAC over the data and then comparing it to
+    # The authenticator can be used at a later time to verify the provenence of
+    # the message by recomputing the HMAC over the message and then comparing it to
     # the provided authenticator.  The class provides methods for generating
     # signatures and also has a constant-time implementation for checking them.
     #
     # This is a secret key authenticator, i.e. anyone who can verify signatures
     # can also create them.
+    #
+    # @see http://nacl.cr.yp.to/auth.html
     class HmacSha512256
       # Number of bytes in a valid key
       KEYBYTES = NaCl::HMACSHA512256_KEYBYTES
@@ -25,68 +27,68 @@ module Crypto
         raise ArgumentError, "Key must be #{KEYBYTES} bytes" unless valid?
       end
 
-      # Compute authenticator for data
+      # Compute authenticator for message
       #
       # @param [#to_str] key the key used for the authenticator
-      # @param [#to_str] data data to construct an authenticator for
+      # @param [#to_str] message message to construct an authenticator for
       #
       # @return [String] The authenticator, as raw bytes
-      def self.auth(key, data)
-        new(key).auth(data)
+      def self.auth(key, message)
+        new(key).auth(message)
       end
 
-      # Verifies the given authenticator with the data.
+      # Verifies the given authenticator with the message.
       #
       # @param [#to_str] key the key used for the authenticator
       # @param [#to_str] authenticator to be checked
-      # @param [#to_str] data the data to be authenticated
+      # @param [#to_str] message the message to be authenticated
       #
       # @return [Boolean] Was it valid?
-      def self.verify(key, authenticator, data)
-        new(key).verify(authenticator, data)
+      def self.verify(key, authenticator, message)
+        new(key).verify(authenticator, message)
       end
 
-      # Compute authenticator for data
+      # Compute authenticator for message
       #
-      # @param [#to_str] data the data to authenticate
+      # @param [#to_str] message the message to authenticate
       #
       # @return [String] The authenticator, as raw bytes
-      def auth(data)
+      def auth(message)
         authenticator = Util.zeros(BYTES)
-        data = data.to_str
-        NaCl.crypto_auth_hmacsha512256(authenticator, data, data.bytesize, @key)
+        message = message.to_str
+        NaCl.crypto_auth_hmacsha512256(authenticator, message, message.bytesize, @key)
         authenticator
       end
 
-      # Compute authenticator for data and hex encode it
+      # Compute authenticator for message and hex encode it
       #
-      # @param [#to_str] data the data to authenticate
+      # @param [#to_str] message the message to authenticate
       #
       # @return [String] The authenticator, hex-encoded
-      def hexauth(data)
-        Util.hexencode(auth(data))
+      def hexauth(message)
+        Util.hexencode(auth(message))
       end
 
-      # Verifies the given authenticator with the data.
+      # Verifies the given authenticator with the message.
       #
       # @param [#to_str] authenticator to be checked
-      # @param [#to_str] data the data to be authenticated
+      # @param [#to_str] message the message to be authenticated
       #
       # @return [Boolean] Was it valid?
-      def verify(authenticator, data)
+      def verify(authenticator, message)
         auth = authenticator.to_str
         return false unless auth.bytesize == BYTES
-        NaCl.crypto_auth_hmacsha512256_verify(authenticator, data, data.bytesize, @key)
+        NaCl.crypto_auth_hmacsha512256_verify(authenticator, message, message.bytesize, @key)
       end
 
-      # Verifies the given hex-authenticator with the data.
+      # Verifies the given hex-authenticator with the message.
       #
       # @param [#to_str] authenticator to be checked, hex encoded
-      # @param [#to_str] data the data to be authenticated
+      # @param [#to_str] message the message to be authenticated
       #
       # @return [Boolean] Was it valid?
-      def hexverify(authenticator, data)
-        verify(Util.hexdecode(authenticator), data)
+      def hexverify(authenticator, message)
+        verify(Util.hexdecode(authenticator), message)
       end
 
       private
