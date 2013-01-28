da1100d28f7780fef3d97c354be3f5777ddde93c
Jonathan Stott <jonathan.stott@gmail.com>
2013-01-28 22:55:49 +0000

    Add bindings to verify_32 and verify_16
    These provide constant time comparison operations for short strings of
    32 and 16 bytes, respectively.  The relevent functions in this library
    already use such comparisons (actually at the C level), but they are
    exposed for completeness.

--- lib/rbnacl/util.rb
+++ lib/rbnacl/util.rb
@@ -54,6 +54,44 @@ module Crypto
     def self.hexdecode(hex)
       [hex].pack("H*")
     end
+
+
+    # Compare two 32 byte strings, in constant time
+    #
+    # This should help to avoid timing attacks for string comparisons in your
+    # application.  Note that many of the functions (such as HmacSha256#verify)
+    # use this method under the hood already.
+    #
+    # @param [String] one String #1
+    # @param [String] two String #2
+    #
+    # @raise [ArgumentError] If the strings are not equal
+    #
+    # @return [Boolean] Well, are they equal?
+    def self.verify_32(one, two)
+      raise(ArgumentError, "First message was #{one.bytesize} bytes, not 32") unless one.bytesize == 32
+      raise(ArgumentError, "Second message was #{two.bytesize} bytes, not 32") unless two.bytesize == 32
+      NaCl.crypto_verify_32(one, two)
+    end
+    
+    
+    # Compare two 16 byte strings, in constant time
+    #
+    # This should help to avoid timing attacks for string comparisons in your
+    # application.  Note that many of the functions (such as OneTime#verify)
+    # use this method under the hood already.
+    #
+    # @param [String] one String #1
+    # @param [String] two String #2
+    #
+    # @raise [ArgumentError] If the strings are not equal
+    #
+    # @return [Boolean] Well, are they equal?
+    def self.verify_16(one, two)
+      raise(ArgumentError, "First message was #{one.bytesize} bytes, not 16") unless one.bytesize == 16
+      raise(ArgumentError, "Second message was #{two.bytesize} bytes, not 16") unless two.bytesize == 16
+      NaCl.crypto_verify_16(one, two)
+    end
   end
 end
 
--- spec/rbnacl/util_spec.rb
+++ spec/rbnacl/util_spec.rb
@@ -9,4 +9,64 @@ describe Crypto::Util do
       described_class.hexdecode(hex).should eq bytes
     end
   end
+
+  context ".verify_32" do
+    let (:msg) { Crypto::Util.zeros(32) }
+    let (:identical_msg) { Crypto::Util.zeros(32) }
+    let (:other_msg) { Crypto::Util.zeros(31) + "\001" }
+    let (:short_msg) { Crypto::Util.zeros(31) }
+    let (:long_msg) { Crypto::Util.zeros(33) }
+
+    it "confirms identical messages are identical" do
+      Crypto::Util.verify_32(msg, identical_msg).should be true
+    end
+
+    it "confirms non-identical messages are non-identical" do
+      Crypto::Util.verify_32(msg, other_msg).should be false
+      Crypto::Util.verify_32(other_msg, msg).should be false
+    end
+
+    it "raises descriptively on a short message in position 1" do
+      expect { Crypto::Util.verify_32(short_msg, msg) }.to raise_error(ArgumentError, /First message was 31 bytes, not 32/)
+    end
+    it "raises descriptively on a short message in position 2" do
+      expect { Crypto::Util.verify_32(msg, short_msg) }.to raise_error(ArgumentError, /Second message was 31 bytes, not 32/)
+    end
+    it "raises descriptively on a long message in position 1" do
+      expect { Crypto::Util.verify_32(long_msg, msg) }.to raise_error(ArgumentError, /First message was 33 bytes, not 32/)
+    end
+    it "raises descriptively on a long message in position 2" do
+      expect { Crypto::Util.verify_32(msg, long_msg) }.to raise_error(ArgumentError, /Second message was 33 bytes, not 32/)
+    end
+  end
+
+  context ".verify_16" do
+    let (:msg) { Crypto::Util.zeros(16) }
+    let (:identical_msg) { Crypto::Util.zeros(16) }
+    let (:other_msg) { Crypto::Util.zeros(15) + "\001" }
+    let (:short_msg) { Crypto::Util.zeros(15) }
+    let (:long_msg) { Crypto::Util.zeros(17) }
+
+    it "confirms identical messages are identical" do
+      Crypto::Util.verify_16(msg, identical_msg).should be true
+    end
+
+    it "confirms non-identical messages are non-identical" do
+      Crypto::Util.verify_16(msg, other_msg).should be false
+      Crypto::Util.verify_16(other_msg, msg).should be false
+    end
+
+    it "raises descriptively on a short message in position 1" do
+      expect { Crypto::Util.verify_16(short_msg, msg) }.to raise_error(ArgumentError, /First message was 15 bytes, not 16/)
+    end
+    it "raises descriptively on a short message in position 2" do
+      expect { Crypto::Util.verify_16(msg, short_msg) }.to raise_error(ArgumentError, /Second message was 15 bytes, not 16/)
+    end
+    it "raises descriptively on a long message in position 1" do
+      expect { Crypto::Util.verify_16(long_msg, msg) }.to raise_error(ArgumentError, /First message was 17 bytes, not 16/)
+    end
+    it "raises descriptively on a long message in position 2" do
+      expect { Crypto::Util.verify_16(msg, long_msg) }.to raise_error(ArgumentError, /Second message was 17 bytes, not 16/)
+    end
+  end
 end
