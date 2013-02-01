96dccb54706adc711e31d1da867839a4514aeb44
Jonathan Stott <jonathan.stott@gmail.com>
2013-02-01 23:20:45 +0000

    Add encoders to sha256 output

--- lib/rbnacl/hash.rb
+++ lib/rbnacl/hash.rb
@@ -6,14 +6,15 @@ module Crypto
     # There's no streaming done, just pass in the data and be done with it.
     #
     # @param [String] data The data, as a collection of bytes
+    # @param [#to_sym] encoding Encoding of the returned hash.
     #
-    # @raise [CryptoError] If the hashing fails for some unknown reason.
+    # @raise [CryptoError] If the hashing fails for some reason.
     #
-    # @return [String] The SHA-256 hash as raw bytes (NOT hex encoded)
-    def self.sha256(data)
+    # @return [String] The SHA-256 hash as raw bytes (Or encoded as per the second argument)
+    def self.sha256(data, encoding = :raw)
       hash = Util.zeros(NaCl::SHA256BYTES)
       NaCl.crypto_hash_sha256(hash, data, data.bytesize) || raise(CryptoError, "Hashing failed!")
-      hash
+      Encoder[encoding].encode(hash)
     end
   end
 end
--- spec/rbnacl/hash_spec.rb
+++ spec/rbnacl/hash_spec.rb
@@ -5,6 +5,8 @@ describe Crypto::Hash do
     let(:reference_string) { "The quick brown fox jumps over the lazy dog." }
     let(:reference_string_hash) { "\xEFS\x7F%\xC8\x95\xBF\xA7\x82Re)\xA9\xB6=\x97\xAAc\x15d\xD5\xD7\x89\xC2\xB7eD\x8C\x865\xFBl" }
     let(:empty_string_hash) { "\xE3\xB0\xC4B\x98\xFC\x1C\x14\x9A\xFB\xF4\xC8\x99o\xB9$'\xAEA\xE4d\x9B\x93L\xA4\x95\x99\exR\xB8U" }
+    let(:reference_string_hash_hex) { reference_string_hash.unpack('H*').first }
+    let(:empty_string_hash_hex) { empty_string_hash.unpack('H*').first }
 
     it "calculates the correct hash for a reference string" do
       Crypto::Hash.sha256(reference_string).should eq reference_string_hash
@@ -12,6 +14,14 @@ describe Crypto::Hash do
 
     it "calculates the correct hash for an empty string" do
       Crypto::Hash.sha256("").should eq empty_string_hash
+    end
+
+    it "calculates the correct hash for a reference string and returns it in hex" do
+      Crypto::Hash.sha256(reference_string, :hex).should eq reference_string_hash_hex
+    end
+
+    it "calculates the correct hash for an empty string and returns it in hex" do
+      Crypto::Hash.sha256("", :hex).should eq empty_string_hash_hex
     end
   end
 end
