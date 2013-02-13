cafb2e3b7fd6dd1377d08850d47caa50b5e7cfd7
Tony Arcieri <tony.arcieri@gmail.com>
2013-02-12 23:25:07 -0800

    Store all test vectors as hex
    After playing around with some conversions on the test vectors, I feel
    it's probably best they're both stored and represented as hex.
    
    This also shows a use case for supporting the encoding API inside
    Crypto::Points. Without it I can't use the nice hex test vectors ;)
    
    I'd call this cleaner and less convoluted in all regards.

--- lib/rbnacl/point.rb
+++ lib/rbnacl/point.rb
@@ -19,10 +19,11 @@ module Crypto
     # Creates a new Point from the given serialization
     #
     # @param value [String] 32-byte value representing a group element
+    # @param encoding [Symbol] The encoding format of the group element
     #
     # @return [Crypto::Point] New Crypto::Point object
-    def initialize(value)
-      value = value.to_s.dup.freeze
+    def initialize(value, encoding = :raw)
+      value = Encoder[encoding].decode(value)
 
       # FIXME: really should have a separate constant here for group element size
       # Group elements and scalars are both 32-bits, but that's for convenience
@@ -34,13 +35,16 @@ module Crypto
     end
 
     # Multiply the given integer by this point
-    # FIXME: This ordering is a bit confusing because traditionally the point
+    # This ordering is a bit confusing because traditionally the point
     # would be the right-hand operand.
     #
     # @param integer [String] 32-byte integer value
+    # @param encoding [Symbol] The encoding format of the integer
     #
     # @return [Crypto::Point] Result as a Point object
-    def mult(integer)
+    def mult(integer, encoding = :raw)
+      integer = Encoder[encoding].decode(integer)
+
       if integer.bytesize != NaCl::SCALARBYTES
         raise ArgumentError, "integer must be exactly #{NaCl::SCALARBYTES} bytes"
       end
