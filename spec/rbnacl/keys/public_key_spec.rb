9ef459b7cf655cc69053f43f67963accf2ed86ab
Tony Arcieri <tony.arcieri@gmail.com>
2013-02-20 22:50:28 -0800

    Centralize Crypto::PrivateKey test vectors

--- spec/rbnacl/keys/public_key_spec.rb
+++ spec/rbnacl/keys/public_key_spec.rb
@@ -1,16 +1,17 @@
 require 'spec_helper'
 
 describe Crypto::PublicKey do
-  let (:alicepk) { "\x85 \xF0\t\x890\xA7Tt\x8B}\xDC\xB4>\xF7Z\r\xBF:\r&8\x1A\xF4\xEB\xA4\xA9\x8E\xAA\x9BNj"  } # from the nacl distribution
-  let (:alicepk_hex) { Crypto::Encoder[:hex].encode(alicepk)  }
-  let (:pk) { Crypto::PublicKey.new(alicepk) }
-  let (:pk_hex) { Crypto::PublicKey.new(alicepk_hex, :hex) }
+  let(:alicepk)     { Crypto::TestVectors[:alice_public] }
+  let(:alicepk_raw) { Crypto::Encoder[:hex].decode(alicepk) }
+
+  subject { Crypto::PublicKey.new(alicepk, :hex) }
+
   context "new" do
     it "accepts a valid key" do
-      expect { Crypto::PublicKey.new(alicepk) }.not_to raise_error
+      expect { Crypto::PublicKey.new(alicepk_raw) }.not_to raise_error
     end
     it "accepts a valid key in hex" do
-      expect { Crypto::PublicKey.new(alicepk_hex, :hex) }.not_to raise_error
+      expect { Crypto::PublicKey.new(alicepk, :hex) }.not_to raise_error
     end
     it "rejects a nil key" do
       expect { Crypto::PublicKey.new(nil) }.to raise_error(ArgumentError)
@@ -22,25 +23,22 @@ describe Crypto::PublicKey do
 
   context "#to_bytes" do
     it "returns the bytes of the key" do
-      pk.to_bytes.should eq alicepk
-    end
-    it "returns the bytes of the key" do
-      pk_hex.to_bytes.should eq alicepk
+      subject.to_bytes.should eq alicepk_raw
     end
   end
 
   context "#to_s" do
     it "returns the bytes of the key" do
-      pk.to_s.should eq alicepk
+      subject.to_s.should eq alicepk_raw
     end
     it "returns the bytes of the key hex encoded" do
-      pk.to_s(:hex).should eq alicepk_hex
+      subject.to_s(:hex).should eq alicepk
     end
   end
   
   include_examples "key equality" do
-    let(:key) { pk }
-    let(:key_bytes) { alicepk }
-    let(:other_key) { described_class.new(alicepk.succ) }
+    let(:key) { subject }
+    let(:key_bytes) { subject.to_bytes }
+    let(:other_key) { described_class.new(alicepk_raw.succ) }
   end
 end
