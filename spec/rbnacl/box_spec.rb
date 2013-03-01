3ab586ada1bb88011ace84c6ff82f80030b3e04b
Tony Arcieri <tony.arcieri@gmail.com>
2013-02-28 23:13:04 -0800

    Centralize Crypto::VerifyKey test vectors

--- spec/rbnacl/box_spec.rb
+++ spec/rbnacl/box_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
 describe Crypto::Box do
-  let (:alicepk) { "\x85 \xF0\t\x890\xA7Tt\x8B}\xDC\xB4>\xF7Z\r\xBF:\r&8\x1A\xF4\xEB\xA4\xA9\x8E\xAA\x9BNj"  } # from the nacl distribution
-  let (:bobsk) { "]\xAB\b~bJ\x8AKy\xE1\x7F\x8B\x83\x80\x0E\xE6o;\xB1)&\x18\xB6\xFD\x1C/\x8B'\xFF\x88\xE0\xEB" } # from the nacl distribution
-  let (:alice_key) { Crypto::PublicKey.new(alicepk) }
-  let (:bob_key) { Crypto::PrivateKey.new(bobsk) }
+  let(:alicepk_hex) { Crypto::TestVectors[:alice_public] }
+  let(:bobsk_hex)   { Crypto::TestVectors[:bob_private] }
+
+  let(:alicepk)   { Crypto::Encoder[:hex].decode(alicepk_hex) }
+  let(:bobsk)     { Crypto::Encoder[:hex].decode(bobsk_hex) }
+  let(:alice_key) { Crypto::PublicKey.new(alicepk) }
+  let(:bob_key)   { Crypto::PrivateKey.new(bobsk) }
 
   context "new" do
     it "accepts strings" do
@@ -31,7 +34,6 @@ describe Crypto::Box do
       expect { Crypto::Box.new(alicepk, "hello") }.to raise_error(Crypto::LengthError, /Private key was 5 bytes \(Expected 32\)/)
     end
   end
-
 
   include_examples 'box' do
     let(:box) { Crypto::Box.new(alicepk, bobsk) }
