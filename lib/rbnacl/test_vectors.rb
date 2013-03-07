b58ada0d0a0e3b56430c30ddfbe9a3b72456285a
Tony Arcieri <tony.arcieri@gmail.com>
2013-03-06 21:57:57 -0800

    Centralize one of the SHA256 test vectors
    The empty string vector is still a literal in the spec

--- lib/rbnacl/test_vectors.rb
+++ lib/rbnacl/test_vectors.rb
@@ -31,6 +31,27 @@ module Crypto
                        "7973f622a43d14a6599b1f654cb45a74e355a5",
 
     #
+    # Ed25519 test vectors
+    # Taken from the Python test vectors: http://ed25519.cr.yp.to/python/sign.input
+    #
+    :sign_private   => "b18e1d0045995ec3d010c387ccfeb984d783af8fbb0f40fa7db126d889f6dadd",
+    :sign_public    => "77f48b59caeda77751ed138b0ec667ff50f8768c25d48309a8f386a2bad187fb",
+    :sign_message   => "916c7d1d268fc0e77c1bef238432573c39be577bbea0998936add2b50a653171" +
+                       "ce18a542b0b7f96c1691a3be6031522894a8634183eda38798a0c5d5d79fbd01" +
+                       "dd04a8646d71873b77b221998a81922d8105f892316369d5224c9983372d2313" +
+                       "c6b1f4556ea26ba49d46e8b561e0fc76633ac9766e68e21fba7edca93c4c7460" +
+                       "376d7f3ac22ff372c18f613f2ae2e856af40",
+    :sign_signature => "6bd710a368c1249923fc7a1610747403040f0cc30815a00f9ff548a896bbda0b" +
+                       "4eb2ca19ebcf917f0f34200a9edbad3901b64ab09cc5ef7b9bcc3c40c0ff7509",
+
+    #
+    # SHA256 test vectors
+    # Taken from the NSRL test vectors: http://www.nsrl.nist.gov/testdata/
+    :sha256_message => "6162636462636465636465666465666765666768666768696768696a68696a6b" +
+                       "696a6b6c6a6b6c6d6b6c6d6e6c6d6e6f6d6e6f706e6f7071",
+    :sha256_digest  => "248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1",
+
+    #
     # Auth test vectors
     # Taken from NaCl distribution
     #
@@ -43,20 +64,6 @@ module Crypto
     :auth_onetime       => "f3ffc7703f9400e52a7dfb4b3d3305d9",
     # self-created (FIXME: find standard test vectors)
     :auth_hmacsha256    => "7f7b9b707e8790ca8620ff94df5e6533ddc8e994060ce310c9d7de04d44aabc3",
-    :auth_hmacsha512256 => "b2a31b8d4e01afcab2ee545b5caf4e3d212a99d7b3a116a97cec8e83c32e107d",
-
-    #
-    # Ed25519 test vectors
-    # Taken from the Python test vectors: http://ed25519.cr.yp.to/python/sign.input
-    #
-    :sign_private   => "b18e1d0045995ec3d010c387ccfeb984d783af8fbb0f40fa7db126d889f6dadd",
-    :sign_public    => "77f48b59caeda77751ed138b0ec667ff50f8768c25d48309a8f386a2bad187fb",
-    :sign_message   => "916c7d1d268fc0e77c1bef238432573c39be577bbea0998936add2b50a653171" +
-                       "ce18a542b0b7f96c1691a3be6031522894a8634183eda38798a0c5d5d79fbd01" +
-                       "dd04a8646d71873b77b221998a81922d8105f892316369d5224c9983372d2313" +
-                       "c6b1f4556ea26ba49d46e8b561e0fc76633ac9766e68e21fba7edca93c4c7460" +
-                       "376d7f3ac22ff372c18f613f2ae2e856af40",
-    :sign_signature => "6bd710a368c1249923fc7a1610747403040f0cc30815a00f9ff548a896bbda0b" +
-                       "4eb2ca19ebcf917f0f34200a9edbad3901b64ab09cc5ef7b9bcc3c40c0ff7509"
+    :auth_hmacsha512256 => "b2a31b8d4e01afcab2ee545b5caf4e3d212a99d7b3a116a97cec8e83c32e107d"
   }
 end
--- spec/rbnacl/hash_spec.rb
+++ spec/rbnacl/hash_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 
 describe Crypto::Hash do
   context "sha256" do
-    let(:reference_string) { "The quick brown fox jumps over the lazy dog." }
-    let(:reference_string_hash) { "\xEFS\x7F%\xC8\x95\xBF\xA7\x82Re)\xA9\xB6=\x97\xAAc\x15d\xD5\xD7\x89\xC2\xB7eD\x8C\x865\xFBl" }
+    let(:reference_string)      { hex2bytes Crypto::TestVectors[:sha256_message] }
+    let(:reference_string_hash) { hex2bytes Crypto::TestVectors[:sha256_digest] }
     let(:empty_string_hash) { "\xE3\xB0\xC4B\x98\xFC\x1C\x14\x9A\xFB\xF4\xC8\x99o\xB9$'\xAEA\xE4d\x9B\x93L\xA4\x95\x99\exR\xB8U" }
     let(:reference_string_hash_hex) { reference_string_hash.unpack('H*').first }
     let(:empty_string_hash_hex) { empty_string_hash.unpack('H*').first }
