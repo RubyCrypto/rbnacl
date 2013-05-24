907bf0c07dc058ad0efbdef47ebc431444c2f696
Tony Arcieri <tony.arcieri@gmail.com>
2013-05-24 08:05:41 -0700

    Bump libsodium version up to 0.4.1

--- lib/rbnacl/rake_tasks.rb
+++ lib/rbnacl/rake_tasks.rb
@@ -3,7 +3,7 @@ require 'rake'
 require 'rake/clean'
 require 'digest/sha2'
 
-LIBSODIUM_VERSION = "0.2"
+LIBSODIUM_VERSION = "0.4.1"
 
 def sh_hidden(command)
   STDERR.puts("*** Executing: #{command}")
@@ -22,7 +22,7 @@ file libsodium_tarball do
   sh "curl -O http://download.dnscrypt.org/libsodium/releases/libsodium-#{LIBSODIUM_VERSION}.tar.gz"
 
   digest = Digest::SHA256.hexdigest(File.read(libsodium_tarball))
-  if digest != "e99a6b69adc080a5acf6b8a49fdc74b61d6f3579b590e85c93446a8325dde100"
+  if digest != "65756c7832950401cc0e6ee0e99b165974244e749f40f33d465f56447bae8ce3"
     rm libsodium_tarball
     raise "#{libsodium_tarball} failed checksum!"
   end
