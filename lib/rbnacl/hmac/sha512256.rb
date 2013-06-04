2d7e1b687da933bc8861c4b100f2a3a74cd57222
Vipul A M <vipulnsward@gmail.com>
2013-06-04 21:33:21 +0530

    Fix some typos

--- lib/rbnacl/hmac/sha512256.rb
+++ lib/rbnacl/hmac/sha512256.rb
@@ -3,7 +3,7 @@ module Crypto
   module HMAC
     # Computes an authenticator as HMAC-SHA-512 truncated to 256-bits
     #
-    # The authenticator can be used at a later time to verify the provenence of
+    # The authenticator can be used at a later time to verify the provenance of
     # the message by recomputing the HMAC over the message and then comparing it to
     # the provided authenticator.  The class provides methods for generating
     # signatures and also has a constant-time implementation for checking them.
