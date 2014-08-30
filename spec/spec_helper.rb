9f0324127c82db4e68d492528d6eaf9379a787fe
Tony Arcieri <bascule@gmail.com>
2014-08-30 14:53:15 -0700

    Disable RSpec monkeypatching

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,12 +23,5 @@ RSpec.configure do |config|
     require 'rbnacl/self_test'
   end
 
-  # Setting this config option `false` removes rspec-core's monkey patching of the
-  # top level methods like `describe`, `shared_examples_for` and `shared_context`
-  # on `main` and `Module`. The methods are always available through the `RSpec`
-  # module like `RSpec.describe` regardless of this setting.
-  # For backwards compatibility this defaults to `true`.
-  #
-  # https://relishapp.com/rspec/rspec-core/v/3-0/docs/configuration/global-namespace-dsl
-  config.expose_dsl_globally = false
+  config.disable_monkey_patching!
 end
