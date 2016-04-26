27f243a91e030dac18273daec9bbf50ee1624a77
Glenn Rempe <glenn@rempe.us>
2016-04-26 14:57:39 -0700

    Expose the internal @signing_key value in RbNaCl::Signatures::Ed25519 as #keypair_bytes

--- spec/rbnacl/signatures/ed25519/signing_key_spec.rb
+++ spec/rbnacl/signatures/ed25519/signing_key_spec.rb
@@ -20,6 +20,11 @@ RSpec.describe RbNaCl::SigningKey do
     expect(subject.to_bytes).to eq signing_key
   end
 
+  it "serializes the internal signing key to bytes" do
+    expect(subject.keypair_bytes.length).to eq 64
+    expect(subject.keypair_bytes).to eq "\xB1\x8E\x1D\x00E\x99^\xC3\xD0\x10\xC3\x87\xCC\xFE\xB9\x84\xD7\x83\xAF\x8F\xBB\x0F@\xFA}\xB1&\xD8\x89\xF6\xDA\xDDw\xF4\x8BY\xCA\xED\xA7wQ\xED\x13\x8B\x0E\xC6g\xFFP\xF8v\x8C%\xD4\x83\t\xA8\xF3\x86\xA2\xBA\xD1\x87\xFB"
+  end
+
   include_examples "key equality" do
     let(:key_bytes) { signing_key }
     let(:key)       { described_class.new(key_bytes) }
