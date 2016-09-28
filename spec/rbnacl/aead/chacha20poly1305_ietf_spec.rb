4cced0b0ed37ce7ae1b81ee4bae9b29348071076
Andrew Davidson <andrew@clearbluetechnologies.com>
2016-09-28 15:51:07 -0400

    Added specs for IETF implementation, DRYed up specs

---
+++ spec/rbnacl/aead/chacha20poly1305_ietf_spec.rb
@@ -0,0 +1,14 @@
+# encoding: binary
+require "spec_helper"
+
+RSpec.describe RbNaCl::AEAD::Chacha20Poly1305IETF do
+  include_examples "aead" do
+    let(:key) {vector :aead_chacha20poly1305_ietf_key}
+    let(:message) {vector :aead_chacha20poly1305_ietf_message}
+    let(:nonce) {vector :aead_chacha20poly1305_ietf_nonce}
+    let(:ad) {vector :aead_chacha20poly1305_ietf_ad}
+    let(:ciphertext) {vector :aead_chacha20poly1305_ietf_ciphertext}
+
+    let(:aead) { RbNaCl::AEAD::Chacha20Poly1305IETF.new(key) }
+  end
+end
---
+++ spec/rbnacl/aead/chacha20poly1305_orig_spec.rb
@@ -0,0 +1,14 @@
+# encoding: binary
+require "spec_helper"
+
+RSpec.describe RbNaCl::AEAD::Chacha20Poly1305 do
+  include_examples "aead" do
+    let(:key) {vector :aead_chacha20poly1305_orig_key}
+    let(:message) {vector :aead_chacha20poly1305_orig_message}
+    let(:nonce) {vector :aead_chacha20poly1305_orig_nonce}
+    let(:ad) {vector :aead_chacha20poly1305_orig_ad}
+    let(:ciphertext) {vector :aead_chacha20poly1305_orig_ciphertext}
+
+    let(:aead) { RbNaCl::AEAD::Chacha20Poly1305.new(key) }
+  end
+end
