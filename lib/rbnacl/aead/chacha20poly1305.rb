b08ef8a554a33c03073ce3209aaeb6caa82b4a4d
Andrew Davidson <andrew@clearbluetechnologies.com>
2016-09-28 13:12:28 -0400

    Added docs

--- lib/rbnacl/aead/chacha20poly1305.rb
+++ lib/rbnacl/aead/chacha20poly1305.rb
@@ -19,34 +19,64 @@ module RbNaCl
                       [:pointer, :pointer, :pointer, :pointer, :ulong_long, :pointer, :ulong_long, :pointer, :pointer]
 
 
+      # Create a new AEAD using the original chacha20poly1305 construction
+      #
+      # Sets up the AEAD with a secret key for encrypting and decrypting messages.
+      #
+      # @param key [String] The key to encrypt and decrypt with
+      #
+      # @raise [RbNaCl::LengthError] on invalid keys
+      #
+      # @return [RbNaCl::AEAD::Chacha20Poly1305IETF] The new AEAD Box, ready to use
+      def initialize(key)
+        @key = Util.check_string(key, KEYBYTES, "Secret key")
+      end
+
+
       # Encrypts and authenticates a message with additional data
-      def encrypt(key, nonce, message, additional_data)
+      #
+      # @param nonce [String] An 8-byte string containing the nonce.
+      # @param message [String] The message to be encrypted.
+      # @param additional_data [String] The additional authenticated data
+      #
+      # @raise [RbNaCl::LengthError] If the nonce is not valid
+      # @raise [RbNaCl::CryptoError] If the ciphertext cannot be authenticated.
+      #
+      # @return [String] The decrypted message (BINARY encoded)
+      def encrypt(nonce, message, additional_data)
+        Util.check_length(nonce, NPUBBYTES, "Nonce")
+
         ciphertext_len = Util.zeros(1)
         ciphertext = Util.zeros(message.bytesize + ABYTES)
-
-        Util.check_length(nonce, NPUBBYTES, "Nonce")
-        key = Util.check_string(key, KEYBYTES, "Secret key")
 
         success = self.class.aead_chacha20poly1305_encrypt(ciphertext, ciphertext_len,
                                                            message, message.bytesize,
                                                            additional_data, additional_data.bytesize,
-                                                           nil, nonce, key)
+                                                           nil, nonce, @key)
         raise CryptoError, "Encryption failed" unless success
         ciphertext
       end
 
       # Decrypts and verifies an encrypted message with additional data
-      def decrypt(key, nonce, ciphertext, additional_data)
+      #
+      # @param nonce [String] An 8-byte string containing the nonce.
+      # @param ciphertext [String] The message to be decrypted.
+      # @param additional_data [String] The additional authenticated data
+      #
+      # @raise [RbNaCl::LengthError] If the nonce is not valid
+      # @raise [RbNaCl::CryptoError] If the ciphertext cannot be authenticated.
+      #
+      # @return [String] The decrypted message (BINARY encoded)
+      def decrypt(nonce, ciphertext, additional_data)
+        Util.check_length(nonce, NPUBBYTES, "Nonce")
+
         message_len = Util.zeros(1)
         message = Util.zeros(ciphertext.bytesize - ABYTES)
-
-        Util.check_length(nonce, NPUBBYTES, "Nonce")
-        key = Util.check_string(key, KEYBYTES, "Secret key")
 
         success = self.class.aead_chacha20poly1305_decrypt(message, message_len, nil,
                                                            ciphertext, ciphertext.bytesize,
                                                            additional_data, additional_data.bytesize,
-                                                           nonce, key)
+                                                           nonce, @key)
         raise CryptoError, "Decryption failed. Ciphertext failed verification." unless success
         message
       end
